8e9b1d846e31070b714eb8fc163ad433bf53b5b2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-25 10:44:29 -0500

    Impement port.go +

--- port/port.go
+++ port/port.go
@@ -1,7 +1,12 @@
 package port
 
-import "strconv"
+import (
+	"net"
+	"strconv"
+	"time"
+)
 
 func ScanPort(protocol, hostname string, port int) bool  {
 	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
 }
