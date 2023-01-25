54112c8b4ffe13dd7e955182f5a7cd7e80286f6e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-25 11:04:13 -0500

    Impement main.go +

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	fmt.Println("Port Scanner in Go")
 
 	open := port.ScanPort("tcp", "localhost", 80)
-
 	fmt.Printf("Port open: %t\n", open)
+
+
+	results := port.InitialScan("localhost")
+	fmt.Println(results)
 }
