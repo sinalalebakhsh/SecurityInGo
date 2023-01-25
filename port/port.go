07378830b0e873a3848c458b139a100835b6e3a0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-25 10:45:12 -0500

    Impement port.go +

--- port/port.go
+++ port/port.go
@@ -9,4 +9,8 @@ import (
 func ScanPort(protocol, hostname string, port int) bool  {
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		return false
+	}
 }
